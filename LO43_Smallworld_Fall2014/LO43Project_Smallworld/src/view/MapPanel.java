4e2b7be28c6b8fdb16b57df77c3285e6c7846863
name <romthib@caramail.fr>
2015-01-05 05:53:37 +0100

    the board

--- LO43_Smallworld_Fall2014/LO43Project_Smallworld/src/view/MapPanel.java
+++ LO43_Smallworld_Fall2014/LO43Project_Smallworld/src/view/MapPanel.java
@@ -3,6 +3,7 @@ package view;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -53,31 +54,24 @@ public class MapPanel extends JPanel{
 		JLabel tmp;
 		initialiseBorder();
 		Random rand = new Random();
-		nbCases=29;
-		
+		nbCases=30;
+		this.setLayout(new GridLayout(0,5));
 		board=new ArrayList<JLabel>();
 		typeOfArea=new ArrayList<Integer>();
 		attribute=new ArrayList<Integer>();
-		for (i=0;i<29;i++)
+		for (i=0;i<nbCases;i++)
 		{			
 			type= rand.nextInt(5 - 0 + 1);
 			att=rand.nextInt(4-0);
 			typeOfArea.add(type);
 			attribute.add(att);
-			/*tmp.setIcon*/
-			tmp=new JLabel(" "+i+" "+type +" "+att);
-			switch(att)
-			{
-			case 0:tmp.setBorder(greyBorder);break;
-			case 1:tmp.setBorder(greybevelBorder);break;
-			case 2:tmp.setBorder(greyraisebevelBorder);break;
-			case 3:tmp.setBorder(greymatteBorder);break;
-			}
+			tmp=new JLabel();
+			setIcon(type,tmp);
+			settypeBorder(att,tmp);
 			
 			board.add(tmp);
 			this.add(board.get(i));
-		}
-		
+		}		
 		 
 	}
 
@@ -108,4 +102,75 @@ public class MapPanel extends JPanel{
 		greymatteBorder=BorderFactory.createEtchedBorder(Color.gray, Color.black);
 	}
 	
+	private void settypeBorder(int attribute, JLabel tmp)
+	{
+		switch(attribute)
+		{
+		case 0:tmp.setBorder(greyBorder);break;
+		case 1:tmp.setBorder(greybevelBorder);break;
+		case 2:tmp.setBorder(greyraisebevelBorder);break;
+		case 3:tmp.setBorder(greymatteBorder);break;
+		}
+	}
+	
+	private void setIcon(int type, JLabel tmp)
+	{
+		
+		switch(type)
+		{
+		case 0: ImageIcon background = new ImageIcon(new ImageIcon("./LO43_Smallworld_Fall2014/LO43Project_Smallworld/img/salle_info.jpg").getImage().getScaledInstance(100, 120, Image.SCALE_DEFAULT));
+				tmp.setIcon(background);break;
+		case 1: ImageIcon background1 = new ImageIcon(new ImageIcon("./LO43_Smallworld_Fall2014/LO43Project_Smallworld/img/atelier.jpg").getImage().getScaledInstance(100, 120, Image.SCALE_DEFAULT));
+				tmp.setIcon(background1);break;
+		case 2: ImageIcon background2 = new ImageIcon(new ImageIcon("./LO43_Smallworld_Fall2014/LO43Project_Smallworld/img/entreprise.jpg").getImage().getScaledInstance(100, 120, Image.SCALE_DEFAULT));
+				tmp.setIcon(background2);break;
+		case 3: ImageIcon background3 = new ImageIcon(new ImageIcon("./LO43_Smallworld_Fall2014/LO43Project_Smallworld/img/foyer.jpeg").getImage().getScaledInstance(100, 120, Image.SCALE_DEFAULT));
+				tmp.setIcon(background3);break;
+		case 4: ImageIcon background4 = new ImageIcon(new ImageIcon("./LO43_Smallworld_Fall2014/LO43Project_Smallworld/img/amphi.jpg").getImage().getScaledInstance(100, 120, Image.SCALE_DEFAULT));
+				tmp.setIcon(background4);break;
+		case 5: ImageIcon background5 = new ImageIcon(new ImageIcon("./LO43_Smallworld_Fall2014/LO43Project_Smallworld/img/gare.jpg").getImage().getScaledInstance(100, 120, Image.SCALE_DEFAULT));
+				tmp.setIcon(background5);break;
+		}
+	}
+	public void changeBorderColor(String color,int position)
+	{
+		switch(color)
+		{
+		case "red":switch(attribute.get(position))
+					{
+					case 0:board.get(position).setBorder(redBorder);break;
+					case 1:board.get(position).setBorder(redbevelBorder);break;
+					case 2:board.get(position).setBorder(redraisebevelBorder);break;
+					case 3:board.get(position).setBorder(redmatteBorder);break;
+					}break;
+		case "blue":switch(attribute.get(position))
+					{
+					case 0:board.get(position).setBorder(blueBorder);break;
+					case 1:board.get(position).setBorder(bluebevelBorder);break;
+					case 2:board.get(position).setBorder(blueraisebevelBorder);break;
+					case 3:board.get(position).setBorder(bluematteBorder);break;
+					}break;
+		case "green":switch(attribute.get(position))
+					{
+					case 0:board.get(position).setBorder(greenBorder);break;
+					case 1:board.get(position).setBorder(greenbevelBorder);break;
+					case 2:board.get(position).setBorder(greenraisebevelBorder);break;
+					case 3:board.get(position).setBorder(greenmatteBorder);break;
+					}break;
+		case "yellow":switch(attribute.get(position))
+					{
+					case 0:board.get(position).setBorder(yellowBorder);break;
+					case 1:board.get(position).setBorder(yellowbevelBorder);break;
+					case 2:board.get(position).setBorder(yellowraisebevelBorder);break;
+					case 3:board.get(position).setBorder(yellowmatteBorder);break;
+					}break;
+		case "gray":switch(attribute.get(position))
+					{
+					case 0:board.get(position).setBorder(greyBorder);break;
+					case 1:board.get(position).setBorder(greybevelBorder);break;
+					case 2:board.get(position).setBorder(greyraisebevelBorder);break;
+					case 3:board.get(position).setBorder(greymatteBorder);break;
+					}break;
+		}
+	}
 }
